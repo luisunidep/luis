5523298e93748cbfd47fdc92b5b52b3fd4e6051c
jose luis <64223510@lcb.unidep.edu.mx>
2024-09-18 09:03:17 -0700

    prueba 1

--- prueba 1/Form1.cs
+++ prueba 1/Form1.cs
@@ -6,5 +6,26 @@ namespace prueba_1
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var num1 = 7;
+            var num2 = 5;
+            operaciones op= new operaciones();
+            int resultado = op.suma(num1, num2);
+        }
+
+        
+        class operaciones   
+        }
+
+
+
+            public int suma(int a, int b)
+            {
+                int toalsuma = a + b;
+                return toalsuma;
+            }
+        }
+    
+
