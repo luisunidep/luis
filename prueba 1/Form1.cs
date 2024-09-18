ea797eb3e59b44215636b7f51c46a1ade01b8a16
jose luis <64223510@lcb.unidep.edu.mx>
2024-09-18 10:21:05 -0700

    prueba 1

--- prueba 1/Form1.cs
+++ prueba 1/Form1.cs
@@ -1,3 +1,4 @@
+
 namespace prueba_1
 {
     public partial class Form1 : Form
@@ -11,21 +12,40 @@ namespace prueba_1
         {
             var num1 = 7;
             var num2 = 5;
-            operaciones op= new operaciones();
-            int resultado = op.suma(num1, num2);
+            Operaciones op = new Operaciones();
+            int resultado1 = op.suma(num1, num2);
+            int resultado2 = op.resta(num1, num2);
+            int resultado3 = op.multiplicación(num1, num2);
+            int resultado4 = op.divicion(num1, num2);
         }
 
-        
-        class operaciones   
-        }
 
-
-
+        class Operaciones
+        {
             public int suma(int a, int b)
             {
                 int toalsuma = a + b;
                 return toalsuma;
             }
+            public int resta(int a, int b)
+            {
+                int toalresta = a + b;
+                return toalresta;
+            }
+            public int multiplicación(int a, int b)
+            {
+                int toalmultiplicación = a + b;
+                return toalmultiplicación;
+            }
+            public int divicion(int a, int b)
+            {
+                int toaldivicion = a + b;
+                return toaldivicion;
+            }
         }
-    
+    }
+}
+        
+
+
 
