b4424943b05610d0ad787521eeaff580fa5036de
jose luis <64223510@lcb.unidep.edu.mx>
2024-09-21 18:51:45 -0700

    numeros primos

---
+++ mayor y menor/mayor y menor/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mayor_y_menor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            Console.WriteLine("Dame el primer numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame el segundo numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame el tercer numero");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            NumeroMayor(num1, num2, num3);
+            NumeroMenor(num1, num2, num3);
+
+            Console.ReadLine();
+        }
+        private static void NumeroMayor(int num1, int num2, int num3)
+        {
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine(" el primer numero es mayor");
+                }
+                else
+                {
+                    Console.WriteLine(" el tercer numero es mayor");
+                }
+
+            }
+            else if (num2 > num1)
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine(" el segundo numero es mayor");
+                }
+                else
+                {
+                    Console.WriteLine(" el tercer numero es mayor");
+                }
+            }
+            Console.ReadLine();
+        }
+        private static void NumeroMenor(int num1, int num2, int num3)
+        {
+            if (num1 < num2)
+            {
+                if (num1 < num3)
+                {
+                    Console.WriteLine(" el primer numero es menor");
+                }
+                else
+                {
+                    Console.WriteLine(" el tercer numero es menor");
+                }
+
+            }
+            else if (num2 < num1)
+            {
+                if (num2 < num3)
+                {
+                    Console.WriteLine(" el segundo numero es menor");
+                }
+                else
+                {
+                    Console.WriteLine(" el tercer numero es menor");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ numeros primos/numeros primos/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_primos
+{
+    namespace numeros_primos
+    {
+        internal class Program
+        {
+            static void Main()
+            {
+                Console.Write("Ingresa un número: ");
+                if (int.TryParse(Console.ReadLine(), out int limite) && limite >= 1)
+                {
+                    Console.WriteLine("Números desde 1 hasta " + limite + ":");
+
+                    for (int i = 1; i <= limite; i++)
+                    {
+                        if (EsPrimo(i))
+                        {
+                            Console.WriteLine(i + " es primo.");
+                        }
+                        else
+                        {
+                            Console.WriteLine(i + " no es primo.");
+                        }
+                    }
+
+                    // Verificar específicamente los números 3, 5 y 7
+                    MostrarPrimosEspecificos(limite);
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, ingresa un número entero válido mayor o igual a 1.");
+                }
+            }
+
+            static bool EsPrimo(int numero)
+            {
+                if (numero < 2) return false;
+                for (int i = 2; i <= Math.Sqrt(numero); i++)
+                {
+                    if (numero % i == 0) return false;
+                }
+                return true;
+            }
+
+            static void MostrarPrimosEspecificos(int limite)
+            {
+                int[] primosEspecificos = { 3, 5, 7 };
+                foreach (int primo in primosEspecificos)
+                {
+                    // Solo mostrar si el primo es menor que el límite
+                    if (primo <= limite)
+                    {
+                        Console.WriteLine(primo + " es primo.");
+                    }
+                }
+            }
+        }
+    }
+}
+
